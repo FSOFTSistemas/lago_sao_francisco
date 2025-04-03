886545deb6c230772294546280277a7afca85d08
PauloHDuque <phbduque@hotmail.com>
2025-04-03 08:30:23 -0300

    feat: adicionado middlewares nas rotas

--- routes/web.php
+++ routes/web.php
@@ -29,45 +29,41 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::get('/financeiro', [App\Http\Controllers\FinanceiroController::class, 'index'])->name('financeiro');
 
-Route::resource('planoDeConta', PlanoDeContaController::class);
+Route::resource('planoDeConta', PlanoDeContaController::class)->middleware('permission:gerenciar plano de conta');
 
-Route::resource('empresa', EmpresaController::class);
+Route::resource('empresa', EmpresaController::class)->middleware('permission:gerenciar empresa');
 
 Route::resource('usuarios', UserController::class)->middleware('permission:gerenciar usuarios');
 
-Route::resource('bancos', BancoController::class);
+Route::resource('bancos', BancoController::class)->middleware('permission:gerenciar banco');
 
-Route::resource('fornecedor', FornecedorController::class);
+Route::resource('fornecedor', FornecedorController::class)->middleware('permission:gerenciar fornecedor');
 
-Route::resource('contasAPagar', ContasAPagarController::class); 
+Route::resource('contasAPagar', ContasAPagarController::class)->middleware('permission:gerenciar contas a pagar');
 
 Route::get('endereco/{cep}', [EnderecoController::class, 'buscarEnderecoPorCep'])->name('buscarCep');
 
 Route::resource('endereco', EnderecoController::class);
 
-Route::resource('funcionario', FuncionarioController::class);
+Route::resource('funcionario', FuncionarioController::class)->middleware('permission:gerenciar funcionario');
 
-Route::resource('adiantamento', AdiantamentoController::class);
+Route::resource('adiantamento', AdiantamentoController::class)->middleware('permission:gerenciar adiantamento');
 
-Route::resource('caixa', CaixaController::class);
+Route::resource('caixa', CaixaController::class)->middleware('permission:gerenciar caixa');
 
-Route::resource('contaCorrente', ContaCorrenteController::class);
+Route::resource('contaCorrente', ContaCorrenteController::class)->middleware('permission:gerenciar conta corrente');
 
-Route::resource('cliente', ClienteController::class);
+Route::resource('cliente', ClienteController::class)->middleware('permission:gerenciar cliente');
 
-Route::resource('fluxoCaixa', FluxoCaixaController::class);
+Route::resource('fluxoCaixa', FluxoCaixaController::class)->middleware('permission:gerenciar fluxo de caixa');
 
-Route::resource('contasAReceber', ContasAReceberController::class);
+Route::resource('contasAReceber', ContasAReceberController::class)->middleware('permission:gerenciar contas a receber');
 
 Route::resource('venda', VendaController::class);
 
-Route::resource('produto', ProdutoController::class);
+Route::resource('produto', ProdutoController::class)->middleware('permission:gerenciar produto');
 
 Route::resource('vendaItem', VendaItemController::class);
 
