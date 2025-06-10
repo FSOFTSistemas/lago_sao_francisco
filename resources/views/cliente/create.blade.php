bb5743cfdee80a25b76c4eef8d4ec55d18019e53
rudrigozx <rodrigo.leandro.zx@gmail.com>
2025-06-10 11:02:55 -0300

    ajustando visualmente campos de create cliente

--- resources/views/cliente/create.blade.php
+++ resources/views/cliente/create.blade.php
@@ -8,135 +8,143 @@
 @stop
 
 @section('content')
-    <div class="card">
-        <div class="card-body">
-            <form id="createClienteForm" action="{{ isset($cliente) ? route('cliente.update', $cliente->id) : route('cliente.store') }}" method="POST">
-                @csrf
-                @if (isset($cliente))
-                    @method('PUT')
-                @endif
+<div class="card">
+    <div class="card-body">
+        <form id="createClienteForm" action="{{ isset($cliente) ? route('cliente.update', $cliente->id) : route('cliente.store') }}" method="POST">
+            @csrf
+            @if (isset($cliente))
+                @method('PUT')
+            @endif
 
-                <div class="form-group row">
-                    <label class="col-md-3 label-control" for="tipo">* Tipo:</label>
-                    <div class="col-md-3">
-                        <select class="form-control select2" id="tipo" name="tipo" required>
-                            <option value="">Selecione...</option>
-                            <option value="PF" {{ (isset($cliente) && $cliente->tipo == 'PF') ? 'selected' : '' }}>Pessoa Física</option>
-                            <option value="PJ" {{ (isset($cliente) && $cliente->tipo == 'PJ') ? 'selected' : '' }}>Pessoa Jurídica</option>
-                        </select>
-                    </div>
+            <!-- Campo Tipo -->
+            <div class="form-group row">
+                <label class="col-md-3 col-form-label label-control" for="tipo">* Tipo:</label>
+                <div class="col-md-3">
+                    <select class="form-control select2" id="tipo" name="tipo" required>
+                        <option value="">Selecione...</option>
+                        <option value="PF" {{ (isset($cliente) && $cliente->tipo == 'PF') ? 'selected' : '' }}>Pessoa Física</option>
+                        <option value="PJ" {{ (isset($cliente) && $cliente->tipo == 'PJ') ? 'selected' : '' }}>Pessoa Jurídica</option>
+                    </select>
                 </div>
+            </div>
 
-                <!-- Todos os outros campos dentro desta div que será mostrada após selecionar o tipo -->
-                <div id="outrosCampos" class="campos-gerais">
-                    <!-- Campos para Pessoa Física -->
-                    <div id="camposPF" class="conditional-field">
-                        
-                        <div class="form-group row">
-                            <label class="col-md-3 label-control" for="cpf"> CPF:</label>
-                            <div class="col-md-3">
-                                <input type="text" class="form-control" id="cpf" name="cpf_cnpj"
-                                    value="{{ $cliente->cpf_cnpj ?? '' }}">
-                            </div>
-                        </div>
-                        
-                        <div class="form-group row">
-                            <label class="col-md-3 label-control" for="rg">RG:</label>
-                            <div class="col-md-3">
-                                <input type="text" class="form-control" id="rg" name="rg_ie"
-                                    value="{{ $cliente->rg_ie ?? '' }}">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label class="col-md-3 label-control" for="dataNascimento">* Data de Nascimento:</label>
-                            <div class="col-md-3">
-                                <input type="date" class="form-control" id="dataNascimento" name="data_nascimento"
-                                    value="{{ $cliente->data_nascimento ?? '' }}">
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Campos para Pessoa Jurídica -->
-                    <div id="camposPJ" class="conditional-field">
-                        <div class="form-group row">
-                            <label class="col-md-3 label-control" for="cnpj">* CNPJ:</label>
-                            <div class="col-md-3">
-                                <input type="text" class="form-control cnpjteste" id="cnpj" name="cpf_cnpj"
-                                    value="{{ $cliente->cpf_cnpj ?? '' }}">
-                            </div>
-                            <button class="btn btn-outline-primary" type="button" id="btnBuscarCnpj">
-                            <i class="bi bi-search"></i> Buscar CNPJ
-                        </button>
-                        </div>
-                        
-                        <div class="form-group row">
-                            <label class="col-md-3 label-control" for="inscricaoEstadual">Inscrição Estadual:</label>
-                            <div class="col-md-3">
-                                <input type="text" class="form-control" id="inscricaoEstadual" name="rg_ie"
-                                    value="{{ $cliente->rg_ie ?? '' }}">
-                            </div>
-                        </div>
-                    </div>
-
+            <!-- Todos os outros campos -->
+            <div id="outrosCampos" class="campos-gerais">
+                <!-- Campos para Pessoa Física -->
+                <div id="camposPF" class="conditional-field">
                     <div class="form-group row">
-                        <label class="col-md-3 label-control" id="labelNomeRazao">* Nome/Razão Social:</label>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control" id="nomeRazaoSocial" name="nome_razao_social"
-                                value="{{ $cliente->nome_razao_social ?? '' }}" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-md-3 label-control" id="labelApelidoFantasia">* Apelido/Nome Fantasia:</label>
-                        <div class="col-md-4">
-                            <input type="text" class="form-control" id="apelidoNomeFantasia" name="apelido_nome_fantasia"
-                                value="{{ $cliente->apelido_nome_fantasia ?? '' }}" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-md-3 label-control" for="telefone">Contato:</label>
-                        <div class="row col-md-6">
-                            <div class="col-md-3">
-                                <label for="telefone">Telefone:</label>
-                                <input type="text" class="form-control" id="telefone" name="telefone"
-                                    value="{{ $cliente->telefone ?? '' }}">
-                            </div>
-                            <div class="col-md-3">
-                                <label for="whatsapp">Whatsapp:</label>
-                                <input type="text" class="form-control" id="whatsapp" name="whatsapp"
-                                    value="{{ $cliente->whatsapp ?? '' }}">
-                            </div>
+                        <label class="col-md-3 col-form-label label-control" for="cpf"> CPF:</label>
+                        <div class="col-md-3">
+                            <input type="text" class="form-control" id="cpf" name="cpf_cnpj"
+                                value="{{ $cliente->cpf_cnpj ?? '' }}">
                         </div>
                     </div>
                     
                     <div class="form-group row">
-                        <label class="col-md-3 label-control" for="endereco_id">Endereço:</label>
+                        <label class="col-md-3 col-form-label label-control" for="rg">RG:</label>
                         <div class="col-md-3">
-                            <select class="form-control select2" id="endereco_id" name="endereco_id">
-                                <option value="">Selecione</option>
-                                @foreach ($enderecos as $endereco)
-                                    <option value="{{ $endereco->id }}" {{ (isset($cliente) && $cliente->endereco_id == $endereco->id) ? 'selected' : '' }}>
-                                        {{ $endereco->logradouro }}, {{ $endereco->numero }}
-                                    </option>
-                                @endforeach
-                            </select>
+                            <input type="text" class="form-control" id="rg" name="rg_ie"
+                                value="{{ $cliente->rg_ie ?? '' }}">
                         </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label label-control" for="dataNascimento">* Data de Nascimento:</label>
+                        <div class="col-md-3">
+                            <input type="date" class="form-control" id="dataNascimento" name="data_nascimento"
+                                value="{{ $cliente->data_nascimento ?? '' }}">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Campos para Pessoa Jurídica -->
+                <div id="camposPJ" class="conditional-field">
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label label-control" for="cnpj">* CNPJ:</label>
+                        <div class="col-md-3">
+                            <input type="text" class="form-control cnpjteste" id="cnpj" name="cpf_cnpj"
+                                value="{{ $cliente->cpf_cnpj ?? '' }}">
+                        </div>
+                        <div class="col-md-2">
+                            <button class="btn btn-outline-primary" type="button" id="btnBuscarCnpj">
+                                <i class="bi bi-search"></i> Buscar CNPJ
+                            </button>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-3 col-form-label label-control" for="inscricaoEstadual">Inscrição Estadual:</label>
+                        <div class="col-md-3">
+                            <input type="text" class="form-control" id="inscricaoEstadual" name="rg_ie"
+                                value="{{ $cliente->rg_ie ?? '' }}">
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Nome/Razão Social -->
+                <div class="form-group row">
+                    <label class="col-md-3 col-form-label label-control" id="labelNomeRazao">* Nome/Razão Social:</label>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control" id="nomeRazaoSocial" name="nome_razao_social"
+                            value="{{ $cliente->nome_razao_social ?? '' }}" required>
+                    </div>
+                </div>
+
+                <!-- Apelido/Nome Fantasia -->
+                <div class="form-group row">
+                    <label class="col-md-3 col-form-label label-control" id="labelApelidoFantasia">* Apelido/Nome Fantasia:</label>
+                    <div class="col-md-6">
+                        <input type="text" class="form-control" id="apelidoNomeFantasia" name="apelido_nome_fantasia"
+                            value="{{ $cliente->apelido_nome_fantasia ?? '' }}" required>
+                    </div>
+                </div>
+
+                <!-- Contatos (agora lado a lado em telas maiores) -->
+                <div class="form-group row">
+                    <label class="col-md-3 col-form-label label-control">Contato:</label>
+                    <div class="col-md-6 inline-fields">
+                        <div>
+                            <label for="telefone">Telefone:</label>
+                            <input type="text" class="form-control" id="telefone" name="telefone"
+                                value="{{ $cliente->telefone ?? '' }}">
+                        </div>
+                        <div>
+                            <label for="whatsapp">Whatsapp:</label>
+                            <input type="text" class="form-control" id="whatsapp" name="whatsapp"
+                                value="{{ $cliente->whatsapp ?? '' }}">
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- Endereço -->
+                <div class="form-group row">
+                    <label class="col-md-3 col-form-label label-control" for="endereco_id">Endereço:</label>
+                    <div class="col-md-4">
+                        <select class="form-control select2" id="endereco_id" name="endereco_id">
+                            <option value="">Selecione</option>
+                            @foreach ($enderecos as $endereco)
+                                <option value="{{ $endereco->id }}" {{ (isset($cliente) && $cliente->endereco_id == $endereco->id) ? 'selected' : '' }}>
+                                    {{ $endereco->logradouro }}, {{ $endereco->numero }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="col-md-2">
                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#enderecoModal">
                             <i class="fas fa-plus"></i> Novo Endereço
                         </button>
                     </div>
-
-                    <!-- Botão de Salvar -->
-                    <div class="card-footer">
-                        <a href="{{ route('cliente.index') }}" class="btn btn-secondary">Voltar</a>
-                        <button type="submit" class="btn btn-primary">{{ isset($cliente) ? 'Atualizar Cliente' : 'Adicionar Cliente' }}</button>
-                    </div>
                 </div>
-            </form>
-        </div>
+
+                <!-- Botão de Salvar -->
+                <div class="card-footer">
+                    <a href="{{ route('cliente.index') }}" class="btn btn-secondary">Voltar</a>
+                    <button type="submit" class="btn btn-primary">{{ isset($cliente) ? 'Atualizar Cliente' : 'Adicionar Cliente' }}</button>
+                </div>
+            </div>
+        </form>
     </div>
+</div>
     
     <!-- Modal para Novo Endereço -->
     @include('components.endereco-modal')
@@ -160,6 +168,65 @@
     
     .campos-gerais.show {
         display: block;
+    }
+    
+    /* Novos estilos */
+    .form-group.row {
+        margin-bottom: 1.5rem;
+        align-items: center;
+    }
+    
+    .label-control {
+        padding-top: 0.375rem;
+        margin-bottom: 0.5rem;
+        font-weight: 500;
+    }
+    
+    .card-body {
+        padding: 2rem;
+    }
+    
+    .card-footer {
+        padding: 1.5rem 0 0 0;
+        background: transparent;
+        border-top: none;
+        display: flex;
+        justify-content: flex-end;
+        gap: 1rem;
+    }
+    
+    @media (max-width: 768px) {
+        .form-group.row {
+            flex-direction: column;
+            align-items: flex-start;
+        }
+        
+        .col-md-3, .col-md-4, .col-md-6 {
+            width: 100%;
+            max-width: 100%;
+        }
+        
+        .row.col-md-6 > div {
+            width: 100%;
+            margin-bottom: 1rem;
+        }
+        
+        .btn {
+            margin-top: 0.5rem;
+            width: 100%;
+        }
+    }
+    
+    /* Melhorias para campos lado a lado */
+    .inline-fields {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1rem;
+    }
+    
+    .inline-fields > div {
+        flex: 1;
+        min-width: 200px;
     }
 </style>
 @stop
