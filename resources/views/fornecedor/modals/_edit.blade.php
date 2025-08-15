bdd6f0b26464e15c31c2c6eff76b1abe7c4b2f37
PauloHDuque <phbduque@hotmail.com>
2025-08-15 16:20:56 -0300

    feat: campo forma pagamento em fornecedor

--- resources/views/fornecedor/modals/_edit.blade.php
+++ resources/views/fornecedor/modals/_edit.blade.php
@@ -1,4 +1,5 @@
-<div class="modal fade" id="editFornecedorModal{{$fornecedor->id}}" tabindex="-1" aria-labelledby="editFornecedorModalLabel" aria-hidden="true">
+<div class="modal fade" id="editFornecedorModal{{ $fornecedor->id }}" tabindex="-1"
+    aria-labelledby="editFornecedorModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
@@ -6,29 +7,37 @@
                 <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form id="editFornecedorForm" action="{{ route('fornecedor.update',$fornecedor->id) }}" method="POST" >
+                <form id="editFornecedorForm" action="{{ route('fornecedor.update', $fornecedor->id) }}" method="POST">
                     @csrf
                     @method('PUT')
                     <div class="mb-3">
                         <label for="razao_social" class="form-label">Razão Social</label>
-                        <input type="text" class="form-control" name="razao_social" value={{$fornecedor->razao_social}} required>
+                        <input type="text" class="form-control" name="razao_social"
+                            value={{ $fornecedor->razao_social }} required>
                     </div>
                     <div class="mb-3">
                         <label for="nomeFantasia">Nome Fantasia:</label>
-                        <input type="text" class="form-control" id="nomeFantasia" name="nome_fantasia" value="{{$fornecedor->nome_fantasia}}">
-                      </div>
-                      <div class="mb-3">
+                        <input type="text" class="form-control" id="nomeFantasia" name="nome_fantasia"
+                            value="{{ $fornecedor->nome_fantasia }}">
+                    </div>
+                    <div class="mb-3">
                         <label for="cnpj">CNPJ:</label>
-                        <input type="text" class="form-control" id="cnpj" name="cnpj" value="{{$fornecedor->cnpj}}">
-                      </div>
-                      <div class="mb-3">
+                        <input type="text" class="form-control" id="cnpj" name="cnpj"
+                            value="{{ $fornecedor->cnpj }}">
+                    </div>
+                    <div class="mb-3">
                         <label for="inscricaoEstadual">Inscrição Estadual:</label>
-                        <input type="text" class="form-control" id="inscricaoEstadual" name="inscricao_estadual" value="{{$fornecedor->inscricao_estadual}}">
-                      </div>
+                        <input type="text" class="form-control" id="inscricaoEstadual" name="inscricao_estadual"
+                            value="{{ $fornecedor->inscricao_estadual }}">
+                    </div>
+                    <div class="mb-3">
+                        <label for="formaPagamento">Forma de Pagamento:</label>
+                        <input type="text" class="form-control" id="formaPagamento" name="forma_pagamento"
+                            value="{{ $fornecedor->forma_pagamento }}">
+                    </div>
                     <button type="submit" class="btn btn-primary">Salvar</button>
                 </form>
             </div>
         </div>
     </div>
 </div>
-
